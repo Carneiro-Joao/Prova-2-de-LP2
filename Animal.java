886cca97abb05c92e8de98dc9ba08a15088f57c4
João Victor de Oliveira Carneiro <166572195+Carneiro-Joao@users.noreply.github.com>
2024-05-26 15:12:50 -0300

    Animal.java

---
+++ Animal.java
@@ -0,0 +1,13 @@
+public class Animal {
+    private String somCaracteristico;
+
+    public Animal(String som) {
+        this.somCaracteristico = som;
+    }
+
+    public void Comunicar() {
+        System.out.println(this.somCaracteristico);
+    }
+
+    // Demais coisas.
+}
